0ec5ad572559d3634eb816680912d9310dd37f2c
m.kikmunter <M.Kikmunter@gmail.com>
2019-09-18 20:10:30 +0100

    working with process.argv

--- globalProcess.js
+++ globalProcess.js
@@ -1,2 +1,8 @@
 console.log(process.pid);
 console.log(process.versions.node);
+
+console.log(process.argv);
+
+//type node your file name your first name and last name to get results, e.g. node globalProcess Mateusz Kikmunter in my case
+const [, , firstName, lastName] = process.argv;
+console.log(`Your name is ${firstName} ${lastName}`);
