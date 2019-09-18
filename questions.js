d0a6707392515bc5bc0c043d966226871d45ab76
m.kikmunter <M.Kikmunter@gmail.com>
2019-09-18 20:29:43 +0100

    working with standard input

--- questions.js
+++ questions.js
@@ -13,5 +13,25 @@ const ask = (i = 0) => {
 };
 
 ask();
-ask(1);
-ask(2);
+
+const answers = [];
+
+process.stdin.on("data", data => {
+    answers.push(data.toString().trim());
+
+    if (answers.length < questions.length) {
+        ask(answers.length);
+    } else {
+        process.exit();
+    }
+});
+
+process.on("exit", () => {
+    const [name, activity, language] = answers;
+
+    console.log(`
+    Thank you for your answers.
+
+    Go ${activity} ${name}, you can write ${language} code later!!!.
+    `);
+});
