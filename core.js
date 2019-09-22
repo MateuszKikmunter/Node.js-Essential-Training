ec07a81daf06fe1729c3f6273bfd2f311a7d2065
m.kikmunter <M.Kikmunter@gmail.com>
2019-09-22 18:01:41 +0100

    working with readline

--- core.js
+++ core.js
@@ -1,6 +1,18 @@
-const path = require("path");
-const util = require("util");
-const v8 = require("v8");
+// const path = require("path");
+// const util = require("util");
+// const v8 = require("v8");
 
-util.log(path.basename(__filename));
-util.log(v8.getHeapSpaceStatistics());
+// util.log(path.basename(__filename));
+// util.log(v8.getHeapSpaceStatistics());
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("How do you like Node?", answer => {
+    console.log(`Your answer: ${ answer }`);
+});
+
