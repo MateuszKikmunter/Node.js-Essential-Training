9ac45aa78b2bfc7331e25c95daee9cfd2f25bf3d
m.kikmunter <M.Kikmunter@gmail.com>
2019-09-22 18:25:57 +0100

    importing custom modules

--- core.js
+++ core.js
@@ -5,38 +5,24 @@
 // util.log(path.basename(__filename));
 // util.log(v8.getHeapSpaceStatistics());
 
-const readline = require("readline");
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
 // rl.question("How do you like Node?", answer => {
 //     console.log(`Your answer: ${ answer }`);
 // });
 
+const collectAnswers = require("./lib/collectAnswers");
+
 const questions = [
     "What is your name? ",
     "Where do you live? ",
-    "Whare are you going to do with node js? "
+    "What are you going to do with node js? "
 ];
-
-const collectAnswers = (questions, done) => {
-    const answers = [];
-    const [firstQuestion] = questions;
-
-    const questionAnswered = answer => {
-        answers.push(answer);
-        if (answers.length < questions.length) {
-            rl.question(questions[answers.length], questionAnswered);
-        } else {
-            done(answers);
-        }
-    };
-
-    rl.question(firstQuestion, questionAnswered);
-};
 
 collectAnswers(questions, answers => {
     console.log("Thank you for your answers.");
