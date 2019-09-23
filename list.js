08e80bea3c7d2e5f54b5a07265331bc80dbf0263
m.kikmunter <M.Kikmunter@gmail.com>
2019-09-23 20:31:48 +0100

    reading folder content

---
+++ list.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+//read folder contents synchronously
+const files = fs.readdirSync("./assets");
+console.log(files);
+
+//read folder contents async
+fs.readdir("./assets", (err, files) => {
+    if(err) {
+        throw err;
+    }
+
+    console.log("complete");
+    console.log(files);
+});
