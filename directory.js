e3d2db7c42c219704bc33c576ac8038c32824a04
m.kikmunter <M.Kikmunter@gmail.com>
2019-09-23 20:51:47 +0100

    creating folders

---
+++ directory.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+if (fs.existsSync("storage-files")) {
+    console.log("folder already exists");
+} else {
+
+    fs.mkdir("storage-files", error => {
+        if (error) {
+            throw error;
+        }
+
+        console.log("directory created");
+    });
+}
